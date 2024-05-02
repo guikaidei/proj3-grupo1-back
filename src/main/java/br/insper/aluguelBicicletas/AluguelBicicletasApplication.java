e0377786ce9cec6ff1237ded9feece7b463f5788
guikaidei <guigokaidei@gmail.com>
2024-05-02 16:11:05 -0300

    criando rep

---
+++ src/main/java/br/insper/aluguelBicicletas/AluguelBicicletasApplication.java
@@ -0,0 +1,13 @@
+package br.insper.aluguelBicicletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelBicicletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelBicicletasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/insper/aluguelBicicletas/AluguelBicicletasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.insper.aluguelBicicletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelBicicletasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
